65fe0a9de8e0161f04bd8298c54d4b45feb34575
Meliflours <45870058+Meliflours@users.noreply.github.com>
2018-12-14 11:31:58 +0300

    Lol
    Добавили строку №14

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             Console.WriteLine("Знакомство с GitHub");
+            Console.WriteLine("Проверка изменения на сервере");
         }
     }
 }
